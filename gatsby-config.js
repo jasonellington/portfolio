6686a728ebf839361ba3cda3a9c16164c78e025b
Jason Ellington <jelling@HOMC02V90TJHV2V.lan1>
2019-05-07 20:32:18 -0400

    update site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Jason Ellington | Software Engineer`,
+    description: `Freelance web developer that enjoys learning the coolest, latest web development frameworks and modern web architectures.`,
+    author: `@jasontellington`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
